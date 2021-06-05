b8d97d6f2d0a425d8e5f2f30a92285d1730c92c5
test <dinhnguyen250800@gmail.com>
2021-06-05 17:18:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//siddeBar-menu
+function openMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+}
+
+//header-onscroll
+// const close1 = document.getElementById("info");
+// const banner1 = document.getElementById("onscroll-Event");
+// const banner2 = document.getElementById("onscroll-Event2");
+// function myFunction(){
+//     if(document.body.scrollTop >50 || document.documentElement.scrollTop >50){
+//         banner1.style.position = "fixed";
+//         banner1.style.marginTop = 0;
+//         banner1.style.zIndex = 100;
+//         close1.style.display ="none";
+
+//         banner2.style.position = "fixed";
+//         banner2.style.marginTop = 0;
+//         banner2.style.zIndex = 100;
+//     }else{
+//         close1.style.display = "block"
+//         banner1.style.position = "absolute";
+
+//         banner2.style.position = "absolute";
+//     }
+// }
+// window.onscroll = function(){
+//     myFunction();
+// }
